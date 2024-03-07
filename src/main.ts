035782a5a1631bdc82184da2059f69d70b3a7394
vitrinekast <roosschuurmans@gmail.com>
2024-03-07 22:31:03 +0100

    remove the public folder and setup basic SSG

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,29 @@
 import "./styles/_style.scss";
+import { ViteSSG } from 'vite-ssg'
 
-import { createApp } from "vue/dist/vue.esm-bundler";
-import { createRouter, createWebHistory } from "vue-router";
 
 import App from "./App.vue";
 import Test from "./pages/Test.vue";
 import Chapter1 from "./pages/Chapter1.vue";
 import Chapter2 from "./pages/Chapter2.vue";
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: "/chapter/1", component: Chapter1 },
-    { path: "/chapter/2", component: Chapter2 },
-    { path: "/test", component: Test }
-  ],
-});
 
-const app = createApp(App);
+const routes =  [
+  { path: "/chapter/1", component: Chapter1 },
+  { path: "/chapter/2", component: Chapter2 },
+  { path: "/test", component: Test }
+]
 
-app.use(router);
-app.config.errorHandler = (err) => {
-  console.error("Error in main.ts: ", err);
-}
 
-app.mount("#app");
+
+// `export const createApp` is required instead of the original `createApp(App).mount('#app')`
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  { routes },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {
+    // install plugins etc.
+  },
+)
