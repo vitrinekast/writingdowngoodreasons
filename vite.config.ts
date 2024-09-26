35cc3f88f08c66b2efc2a2917c6134a6bfa0f51e
vitrinekast <roosschuurmans@gmail.com>
2024-09-26 18:37:18 +0200

    audit fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import Markdown from 'unplugin-vue-markdown/vite';
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import Pages from 'vite-plugin-pages';
-import { VitePWA } from 'vite-plugin-pwa'
-
 
 export default defineConfig({
   
@@ -19,7 +17,6 @@ export default defineConfig({
         }
       }
     }),
-    VitePWA({ registerType: 'autoUpdate' }),
     ViteImageOptimizer({
       png: {
         quality: 10,
